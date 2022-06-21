27869c45ca41cd67dccfaef90b80376f36c19cf2
prashanttiwari-001 <prashantttiwari976@gmail.com>
2022-06-21 18:10:00 +0530

    chat app

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var socket = require("socket.io");
+
+// app setup
+var app = express();
+var server = app.listen(3000,function(){
+    console.log("listening to the port 3000");
+});
+
+app.use(express.static("public"));
+
+// socket setup
+var io = socket(server);
+
+io.on("connection",function(socket){
+    console.log("made socket connection",socket.id);
+
+    socket.on("chat",function(data){
+        io.sockets.emit("chat",data);
+    });
+
+    socket.on("typing",function(data){
+        socket.broadcast.emit("typing",data);
+    });
+});
